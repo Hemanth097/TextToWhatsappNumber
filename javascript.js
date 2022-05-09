cc4b8dbdd728d3bc83c79c129bf1c7ee6846f5a3
Hemanth097 <64942691+Hemanth097@users.noreply.github.com>
2022-05-09 15:08:20 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,18 @@
-		function sendMessage() {
+		function GenerateURL() {
 
 			var texturl = encodeURIComponent(document.getElementById("wtext").value);
 			var comurl = "https://wa.me/91" + document.getElementById('wno').value + "?text=" + texturl + "";
 
 			document.getElementById("urcode").innerHTML = "Generated url : <a href = " + comurl + " > "+ comurl + "</a>";
            	//window.open("https://wa.me/91" + document.getElementById("wno").value + "?text=" + texturl + "", "_blank");
+
+        }
+           function sendMessage() {
+
+	var texturl = encodeURIComponent(document.getElementById("wtext").value);
+	var comurl = "https://wa.me/91" + document.getElementById('wno').value + "?text=" + texturl + "";
+
+	//document.getElementById("urcode").innerHTML = "Generated url : <a href = " + comurl + " > "+ comurl + "</a>";
+        window.open("https://wa.me/91" + document.getElementById("wno").value + "?text=" + texturl + "", "_blank");
 
         }
